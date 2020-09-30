eb932eb88928262474eedae87f566c2d834e3d05
Torion <teezus318@gmail.com>
2020-09-29 20:51:26 -0500

    Studio: Completed

---
+++ AreaOfACircle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AreaOfACircle
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double area;
+            double pi = System.Math.PI;
+            double radius;
+            string input;
+            string mpg;
+            double numMPG;
+            double gas;
+            double diameter;
+            double circumference;
+
+            Console.WriteLine("Enter a radius:");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+
+            area = (pi * radius * radius);
+
+            Console.WriteLine("The area of a circle of radius " + input + " is: {0:F3}", area);
+
+            circumference = (2 * pi * radius);
+            diameter = (2 * radius);
+            Console.WriteLine("The circumference of this circle is: {0:F3}", circumference);
+            Console.WriteLine("The diameter of this circle is: {0:F3}", diameter);
+
+            Console.WriteLine("What's the miles per gallon of your vehicle? ");
+            mpg = Console.ReadLine();
+            numMPG = double.Parse(mpg);
+
+            gas = circumference / numMPG;
+
+            Console.WriteLine("You would use {0:F2} gallons of gas", gas);
+         
+        }
+    }
+}
